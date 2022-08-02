a874526cb5eaa34130c2b07eb301f7b21a8bede2
zeca.rt22 <zeca.rt22@gmail.com>
2022-08-02 14:04:56 -0300

    feat/EditEvents page done

--- src/pages/EditEvent/index.tsx
+++ src/pages/EditEvent/index.tsx
@@ -1,5 +1,69 @@
-import { Text, useMediaQuery } from "@chakra-ui/react";
+import {
+  Image,
+  VStack,
+  HStack,
+  Box,
+  keyframes,
+  useMediaQuery,
+  Heading,
+} from "@chakra-ui/react";
+import { LoginForm } from "../../components/Forms/Login";
+import imageDestak3 from "../../assets/images/image3.png";
+import logo from "../../assets/images/logo.png";
+import { CardEvents } from "../../components/CardEvents";
+
+const AppearFromRight = keyframes`
+from {opacity: 0;}
+to {transform: translateX(-5px)}
+`;
 
 export const EditEvent = () => {
-  return <Text>Edit Event</Text>;
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
+
+  return (
+    <>
+      {isLargerThan1280 ? (
+        <>
+          <Box bg={"theme.white"} w="194vh">
+            <HStack spacing={150}>
+              <Image
+                src={imageDestak3}
+                alt="banner"
+                h={"100vh"}
+                w={"80vh"}
+                boxShadow={"dark-lg"}
+                animation={`${AppearFromRight} 3s`}
+              />
+              <VStack spacing={10}>
+                <Image src={logo} alt="logo" />
+                <Heading>Editar evento</Heading>
+                <CardEvents
+                  color="#f59c0d"
+                  date={"22-03"}
+                  description={"Comemoração do aniverssário da cidade"}
+                  tittle={"Anivessário da cidade"}
+                />
+              </VStack>
+            </HStack>
+          </Box>
+        </>
+      ) : (
+        <>
+          {/* mobile */}
+          <Box bg={"theme.white"} h="99.2vh">
+            <VStack spacing={10}>
+              <Image src={logo} alt="logo" mt={50} />
+              <Heading>Editar evento</Heading>
+              <CardEvents
+                color="#f59c0d"
+                date={"22-03"}
+                description={"Comemoração do aniverssário da cidade"}
+                tittle={"Anivessário da cidade"}
+              />
+            </VStack>
+          </Box>
+        </>
+      )}
+    </>
+  );
 };
